15795d2b67759e4fe87d51729747f1e4438b5aad
Robert Pietroiu <pietroiurobert21@stud.ase.ro>
2023-12-25 20:11:56 +0200

    more table improvements

--- frontend/src/pages/Contacts/Contacts.jsx
+++ frontend/src/pages/Contacts/Contacts.jsx
@@ -1,4 +1,8 @@
 import { Table } from 'evergreen-ui'
+import { Combobox } from 'evergreen-ui'
+import { TrashIcon } from 'evergreen-ui'
+import { Button } from 'evergreen-ui'
+
 
 export default function Contacts() {
     const profiles = [
@@ -21,14 +25,114 @@ export default function Contacts() {
             email: "doe2sean@gmail.com",
             phoneNumber: "1234567890",
             status: "lead"
-        }
+        },
+        {
+            id: 2,
+            name: "Sean",
+            lastName: "Doe",
+            proffesionalTitle: "sales manager",
+            company: "Google",
+            email: "doe2sean@gmail.com",
+            phoneNumber: "1234567890",
+            status: "lead"
+        },
+        {
+            id: 2,
+            name: "Sean",
+            lastName: "Doe",
+            proffesionalTitle: "sales manager",
+            company: "Google",
+            email: "doe2sean@gmail.com",
+            phoneNumber: "1234567890",
+            status: "lead"
+        },
+        {
+            id: 2,
+            name: "Sean",
+            lastName: "Doe",
+            proffesionalTitle: "sales manager",
+            company: "Google",
+            email: "doe2sean@gmail.com",
+            phoneNumber: "1234567890",
+            status: "lead"
+        },
+        {
+            id: 2,
+            name: "Sean",
+            lastName: "Doe",
+            proffesionalTitle: "sales manager",
+            company: "Google",
+            email: "doe2sean@gmail.com",
+            phoneNumber: "1234567890",
+            status: "lead"
+        },
+        {
+            id: 2,
+            name: "Sean",
+            lastName: "Doe",
+            proffesionalTitle: "sales manager",
+            company: "Google",
+            email: "doe2sean@gmail.com",
+            phoneNumber: "1234567890",
+            status: "lead"
+        },
+        {
+            id: 2,
+            name: "Sean",
+            lastName: "Doe",
+            proffesionalTitle: "sales manager",
+            company: "Google",
+            email: "doe2sean@gmail.com",
+            phoneNumber: "1234567890",
+            status: "lead"
+        },
+        {
+            id: 2,
+            name: "Sean",
+            lastName: "Doe",
+            proffesionalTitle: "sales manager",
+            company: "Google",
+            email: "doe2sean@gmail.com",
+            phoneNumber: "1234567890",
+            status: "lead"
+        },
+        {
+            id: 2,
+            name: "Sean",
+            lastName: "Doe",
+            proffesionalTitle: "sales manager",
+            company: "Google",
+            email: "doe2sean@gmail.com",
+            phoneNumber: "1234567890",
+            status: "lead"
+        },
+        {
+            id: 2,
+            name: "Sean",
+            lastName: "Doe",
+            proffesionalTitle: "sales manager",
+            company: "Google",
+            email: "doe2sean@gmail.com",
+            phoneNumber: "1234567890",
+            status: "lead"
+        },
+        {
+            id: 2,
+            name: "Sean",
+            lastName: "Doe",
+            proffesionalTitle: "sales manager",
+            company: "Google",
+            email: "doe2sean@gmail.com",
+            phoneNumber: "1234567890",
+            status: "lead"
+        },
     ]
 
     return (
-        <>
+        <>           
             <Table style={{width:"100vw", padding:"2%", paddingTop: "0"}}>
                 <Table.Head>
-                    <Table.SearchHeaderCell />
+                    <Table.SearchHeaderCell style={{width:"1rem"}}/>
                     <Table.TextHeaderCell>First Name</Table.TextHeaderCell>
                     <Table.TextHeaderCell>Last Name</Table.TextHeaderCell>
                     <Table.TextHeaderCell>Professional Title</Table.TextHeaderCell>
@@ -36,11 +140,12 @@ export default function Contacts() {
                     <Table.TextHeaderCell>Email Address</Table.TextHeaderCell>
                     <Table.TextHeaderCell>Phone Number</Table.TextHeaderCell>
                     <Table.TextHeaderCell>Status</Table.TextHeaderCell>
+                    <Table.TextHeaderCell> </Table.TextHeaderCell>
                 </Table.Head>
 
-                <Table.VirtualBody height={240}>
+                <Table.VirtualBody height={440}>
                      {profiles.map((profile) => (
-                        <Table.Row key={profile.id} isSelectable onSelect={() => alert(profile.name)}>
+                        <Table.Row key={profile.id} isSelectable onSelect={() => {}}>
                             <Table.TextCell>{profile.id}</Table.TextCell>
                             <Table.TextCell>{profile.name}</Table.TextCell>
                             <Table.TextCell>{profile.lastName}</Table.TextCell>
@@ -48,11 +153,26 @@ export default function Contacts() {
                             <Table.TextCell>{profile.company}</Table.TextCell>
                             <Table.TextCell>{profile.email}</Table.TextCell>
                             <Table.TextCell>{profile.phoneNumber}</Table.TextCell>
-                            <Table.TextCell>{profile.status}</Table.TextCell>
+
+                            <Table.TextCell>     
+                                <Combobox
+                                    initialSelectedItem={{ label: 'customer' }}
+                                    items={[{ label: 'prospect' }, { label: 'lead' }, { label: 'contacted' }, { label: 'customer'}]}
+                                    itemToString={item => (item ? item.label : '')}
+                                    onChange={selected => console.log(selected)}
+                                    style={{width:"7rem"}}
+                                />
+                            </Table.TextCell>
+
+                            {/* <Table.TextCell>{profile.status}</Table.TextCell> */}
+
+                            <Table.TextCell> <TrashIcon onClick={(event)=>{alert('delete');event.stopPropagation()}}/> </Table.TextCell>
                         </Table.Row>
                     ))} 
                 </Table.VirtualBody>
             </Table>
+            
+            <Button appearance="default" intent="none" style={{left:"2%"}}>Add Contact</Button>
         </>
     )
 }
