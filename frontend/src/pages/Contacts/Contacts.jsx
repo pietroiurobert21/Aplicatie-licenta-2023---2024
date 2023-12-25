dba517b2829d96fd6bb6015a6ed3f46c46e3e629
Robert Pietroiu <pietroiurobert21@stud.ase.ro>
2023-12-25 19:41:46 +0200

    some contacts table improvements

--- frontend/src/pages/Contacts/Contacts.jsx
+++ frontend/src/pages/Contacts/Contacts.jsx
@@ -1,29 +1,57 @@
 import { Table } from 'evergreen-ui'
 
 export default function Contacts() {
+    const profiles = [
+        {
+            id: 1,
+            name: "John",
+            lastName: "Michael",
+            proffesionalTitle: "marketing specialist",
+            company: "Google",
+            email: "jmichael@yahoo.com",
+            phoneNumber: "1234567890",
+            status: "lead"
+        },
+        {
+            id: 2,
+            name: "Sean",
+            lastName: "Doe",
+            proffesionalTitle: "sales manager",
+            company: "Google",
+            email: "doe2sean@gmail.com",
+            phoneNumber: "1234567890",
+            status: "lead"
+        }
+    ]
+
     return (
         <>
             <Table style={{width:"100vw", padding:"2%", paddingTop: "0"}}>
                 <Table.Head>
                     <Table.SearchHeaderCell />
-                    <Table.TextHeaderCell>Last Activity</Table.TextHeaderCell>
-                    <Table.TextHeaderCell>ltv</Table.TextHeaderCell>
-                    </Table.Head>
-                    <Table.VirtualBody height={240}>
-    {/* {profiles.map((profile) => (
-      <Table.Row key={profile.id} isSelectable onSelect={() => alert(profile.name)}>
-        <Table.TextCell>{profile.name}</Table.TextCell>
-        <Table.TextCell>{profile.lastActivity}</Table.TextCell>
-        <Table.TextCell isNumber>{profile.ltv}</Table.TextCell>
-      </Table.Row>
-    ))} */}
+                    <Table.TextHeaderCell>First Name</Table.TextHeaderCell>
+                    <Table.TextHeaderCell>Last Name</Table.TextHeaderCell>
+                    <Table.TextHeaderCell>Professional Title</Table.TextHeaderCell>
+                    <Table.TextHeaderCell>Company</Table.TextHeaderCell>
+                    <Table.TextHeaderCell>Email Address</Table.TextHeaderCell>
+                    <Table.TextHeaderCell>Phone Number</Table.TextHeaderCell>
+                    <Table.TextHeaderCell>Status</Table.TextHeaderCell>
+                </Table.Head>
 
-                        <Table.Row key={"1"} isSelectable onSelect={() => alert("profile.name")}>
-                            <Table.TextCell>John</Table.TextCell>
-                            <Table.TextCell>Michael</Table.TextCell>
-                            <Table.TextCell isNumber>profile.ltv</Table.TextCell>
+                <Table.VirtualBody height={240}>
+                     {profiles.map((profile) => (
+                        <Table.Row key={profile.id} isSelectable onSelect={() => alert(profile.name)}>
+                            <Table.TextCell>{profile.id}</Table.TextCell>
+                            <Table.TextCell>{profile.name}</Table.TextCell>
+                            <Table.TextCell>{profile.lastName}</Table.TextCell>
+                            <Table.TextCell>{profile.proffesionalTitle}</Table.TextCell>
+                            <Table.TextCell>{profile.company}</Table.TextCell>
+                            <Table.TextCell>{profile.email}</Table.TextCell>
+                            <Table.TextCell>{profile.phoneNumber}</Table.TextCell>
+                            <Table.TextCell>{profile.status}</Table.TextCell>
                         </Table.Row>
-                    </Table.VirtualBody>
+                    ))} 
+                </Table.VirtualBody>
             </Table>
         </>
     )
