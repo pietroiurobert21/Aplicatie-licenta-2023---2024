e12bf7fb099a59a101fabf1ef3e4c5218d8d653f
Robert Pietroiu <pietroiurobert21@stud.ase.ro>
2023-12-27 13:33:26 +0200

    env

--- frontend/src/components/Chatbot/Chatbot.jsx
+++ frontend/src/components/Chatbot/Chatbot.jsx
@@ -9,10 +9,10 @@ const Chatbot = () => {
  
     script.onload = () => {
       window.botpressWebChat.init({
-        botId: '11e2b658-cb75-4ec2-8b3b-a315559f6743',
+        botId: import.meta.env.VITE_botID,
         hostUrl: 'https://cdn.botpress.cloud/webchat/v1',
         messagingUrl: 'https://messaging.botpress.cloud',
-        clientId: '11e2b658-cb75-4ec2-8b3b-a315559f6743',
+        clientId: import.meta.env.VITE_clientId,
       })
     }
   }, [])
